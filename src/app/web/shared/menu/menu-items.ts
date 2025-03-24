c5f88e8761550a60d3591e4672ff6177e98f4f84
FabricioWashington <fabriciowashington2015@gmail.com>
2025-03-23 23:34:42 -0300

    trocando icon

--- src/app/web/shared/menu/menu-items.ts
+++ src/app/web/shared/menu/menu-items.ts
@@ -1,6 +1,6 @@
 export const MENU_ITEMS = [
   { label: 'Home', route: '/home', icon: 'house' },
-  { label: 'Ver Agendamentos', route: '/agendamento/view', icon: 'medication' },
+  { label: 'Ver Agendamentos', route: '/agendamento/view', icon: 'calendar_month' },
   { label: 'Ver Pacientes', route: '/pacientes/view', icon: 'patient_list' },
   { label: 'Ver Medicos', route: '/medicos/view', icon: 'stethoscope_check' },
   { label: 'Ver Especialidades', route: '/especialidade/view', icon: 'demography' },
